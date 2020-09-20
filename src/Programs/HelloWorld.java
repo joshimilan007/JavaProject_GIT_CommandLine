a4b3d5ae416d21ba82808cdbe020060d38895501
Milan Joshi <milan.joshi@soprasteria.com>
2020-09-20 23:22:23 +0530

    new change added in HelloWorld.java

--- src/Programs/HelloWorld.java
+++ src/Programs/HelloWorld.java
@@ -5,5 +5,11 @@ public class HelloWorld {
 	public static void main(String[] s) {
 		System.out.println("Hello world");
 	}
+	
+	
+	public static void newchangefunction()
+	{
+		System.out.println("new change");
+	}
 
 }
